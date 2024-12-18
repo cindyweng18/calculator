30990677c9991b70e4769f2db821103846bb2b06
cindyweng18 <cindyweng18@gmail.com>
2024-12-18 17:41:05 -0500

    create basic calculator functions

--- script.js
+++ script.js
@@ -1,30 +1,56 @@
 let input = "";
+const display = document.getElementById("display");
 
 for (let i = 0; i < 10; i++) {
     const numberButton = document.createElement("button");
     numberButton.innerHTML = i.toString();
     numberButton.addEventListener("click", function() {
         input = input + i.toString();
-        const display = document.getElementById("display");
         display.innerHTML = input;
 
     })
     document.body.appendChild(numberButton);
 }
 
+function equal() {
+    const l = input.split(" ");
+    const a = parseInt(l[0]);
+    const b = parseInt(l[2]);
+    let result;
+    switch(l[1]) {
+        case "+":
+            result = a + b;
+            break;
+        case "-":
+            result = a - b;
+            break;
+        case "*":
+            result = a * b;
+            break;
+        case "/":
+            result = a / b;
+            break;
+    }
+    display.innerHTML = result;
+}
+
 
 function add() {
-    alert("adding");
+    input = input + " + ";
+    display.innerHTML = input;
 }
 
 function subtract() {
-    alert("subtracting");
+    input = input + " - ";
+    display.innerHTML = input;
 }
 
 function multiply() {
-    alert("multiplying");
+    input = input + " * ";
+    display.innerHTML = input;
 }
 
 function divide() {
-    alert("dividing");
+    input = input + " / ";
+    display.innerHTML = input;
 }
